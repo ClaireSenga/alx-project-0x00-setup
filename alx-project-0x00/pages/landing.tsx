1ed66dd5571387f37e21b6bf5af08f458cf90a96
“ClaireSenga” <marieclairemangue2908@gmail>
2025-05-31 18:46:16 +0200

    last

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import Card from "@/components/Card";
+import Button from "@/components/Button"
 
 const Landing: React.FC = () => {
   return (
     <div>
       <h1 className="text-xl font-extralight">Landing Page</h1>
 	<Card />
+
+	{/* Small buttons */}
+      <div className="mt-4 space-x-2">
+        <Button 
+          title="Small / Rounded-sm" 
+          styles="px-2 py-1 bg-blue-500 text-white text-sm rounded-sm"
+	/>
     </div>
   );
 };
